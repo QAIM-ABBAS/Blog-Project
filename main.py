022e3ffff771ce3e712b049f6b1368dfc022f86f
Qaim Abbas <salam4491@gmial.com>
2024-06-08 17:14:16 +0330

    initial commit

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from functools import wraps
 from datetime import date
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = "don't_tell_any1"
 login_manager = LoginManager(app)
 ckeditor = CKEditor(app)
 app.app_context().push()
